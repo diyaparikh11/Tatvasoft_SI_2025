c6c02249120c5324c6e491912d0e5b5775443c41
Diya <diyakrishaparikh@gmail.com>
2025-07-11 15:48:17 +1400

    Uploaded updated Mission files to Day 5 folder

---
+++ Day 5/Mission/Mission.Api/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mission.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DashboardController : ControllerBase
+    {
+        [Authorize(Roles = "Admin")]
+        [HttpGet("admin-dashboard")]
+        public IActionResult AdminDashboard()
+        {
+            return Ok("Hello Admin");
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpGet("user-dashboard")]
+        public IActionResult UserDashboard()
+        {
+            return Ok("Hello User");
+        }
+    }
+}
---
+++ Day 5/Mission/Mission.Entitle/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Mission.Entities
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MissionDBContext>
+    {
+        public MissionDBContext CreateDbContext(string[] args)
+        {
+            // 👇 Dynamically get path to Mission.Api where appsettings.json is
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../Mission.Api");
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath) // 👈 this is the fix
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<MissionDBContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new MissionDBContext(builder.Options);
+        }
+    }
+}
